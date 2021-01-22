bb6ad04729f333e50bb25a05188946f191ed6034
Kevin <kevin.yi93@gmail.com>
2021-01-22 14:38:28 -0500

    Create GET route for retrieving crypto data

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,38 @@
 const express = require('express');
-const os = require('os');
+const axios = require('axios');
+require('dotenv').config();
+
+const API_KEY = process.env.CMC_API_KEY;
 
 const app = express();
 
 app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+
+app.get('/api/getCryptoData', (req, res) => {
+  const request = {
+    method: 'get',
+    url: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
+    headers: {
+      'X-CMC_PRO_API_KEY': API_KEY
+    },
+    json: true,
+    gzip: true,
+    params: {
+      'start': "1",
+      'limit': "2",
+      'convert': 'USD'
+    }
+  };
+
+  axios(request)
+    .then(response => {
+      const { data } = response;
+      res.status(200).json(data);
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(400).json({ error });
+    });
+});
 
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
