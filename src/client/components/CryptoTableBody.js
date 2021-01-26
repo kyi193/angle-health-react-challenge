1ce67f1228a07d87e1d7d66bbd10988ec93c87aa
Kevin <kevin.yi93@gmail.com>
2021-01-26 16:49:32 -0500

    Refactor variable names

--- src/client/components/CryptoTableBody.js
+++ src/client/components/CryptoTableBody.js
@@ -22,66 +22,75 @@ const CryptoTableBody = ({cryptoList}) => {
   };
 
   return (
-    cryptoList.map((cryptocurrency, i) => (
-      <TableRow className={styles.cryptoRow} key={i}>
-        <TableCell component='th' scope='row'>
-          <Typography>{i + 1}</Typography>
-        </TableCell>
-        <TableCell component='th' scope='row'>
-          <div className={styles.nameContainer}>
-            <img className={styles.logo} src={logoMap[cryptocurrency.symbol]} alt={`${cryptocurrency.name} logo`} />
-            <p className={styles.dataBoldText}>{cryptocurrency.name}</p>
-            <p className={styles.dataGrayText}>{cryptocurrency.symbol}</p>
-          </div>
-        </TableCell>
-        <TableCell component='th' scope='row'>
-          <Typography>${parseIntegerWithCommas(cryptocurrency.quote.USD.price)}</Typography>
-        </TableCell>
-        <TableCell component='th' scope='row'>
-          <Typography
-            className={parseIntegerWithCommas(cryptocurrency.quote.USD.percent_change_24h) >= 0 ?
-              styles.dataGreenText :
-              styles.dataRedText}
-          >
-            {parseIntegerWithCommas(cryptocurrency.quote.USD.percent_change_24h) >= 0 ?
-              <ArrowDropUp /> :
-              <ArrowDropDown />}
-            {parseIntegerWithCommas(cryptocurrency.quote.USD.percent_change_24h)}%
+    cryptoList.map((cryptocurrency, index) => {
+      const { name, symbol, circulating_supply, total_supply } = cryptocurrency;
+      const price = parseIntegerWithCommas(cryptocurrency.quote.USD.price);
+      const percent_change_24h = parseIntegerWithCommas(cryptocurrency.quote.USD.percent_change_24h);
+      const percent_change_7d = parseIntegerWithCommas(cryptocurrency.quote.USD.percent_change_7d);
+      const market_cap = parseIntegerWithCommas(cryptocurrency.quote.USD.market_cap);
+      const volume_24h = parseIntegerWithCommas(cryptocurrency.quote.USD.volume_24h);
+
+      return (
+        <TableRow className={styles.cryptoRow} key={index}>
+          <TableCell component='th' scope='row'>
+            <Typography>{index + 1}</Typography>
+          </TableCell>
+          <TableCell component='th' scope='row'>
+            <div className={styles.nameContainer}>
+              <img className={styles.logo} src={logoMap[symbol]} alt={`${name} logo`} />
+              <p className={styles.dataBoldText}>{name}</p>
+              <p className={styles.dataGrayText}>{symbol}</p>
+            </div>
+          </TableCell>
+          <TableCell component='th' scope='row'>
+            <Typography>${price}</Typography>
+          </TableCell>
+          <TableCell component='th' scope='row'>
+            <Typography
+              className={percent_change_24h >= 0 ?
+                styles.dataGreenText :
+                styles.dataRedText}
+            >
+              {percent_change_24h >= 0 ?
+                <ArrowDropUp /> :
+                <ArrowDropDown />}
+              {percent_change_24h}%
+            </Typography>
+          </TableCell>
+          <TableCell component='th' scope='row'>
+            <Typography
+              className={percent_change_7d >= 0 ?
+                styles.dataGreenText :
+                styles.dataRedText}
+            >
+              {percent_change_7d >= 0 ?
+                <ArrowDropUp /> :
+                <ArrowDropDown />}
+              {Math.abs(percent_change_7d)}%
           </Typography>
-        </TableCell>
-        <TableCell component='th' scope='row'>
-          <Typography
-            className={parseIntegerWithCommas(cryptocurrency.quote.USD.percent_change_7d) >= 0 ?
-              styles.dataGreenText :
-              styles.dataRedText}
-          >
-            {parseIntegerWithCommas(cryptocurrency.quote.USD.percent_change_7d) >= 0 ?
-              <ArrowDropUp /> :
-              <ArrowDropDown />}
-            {Math.abs(parseIntegerWithCommas(cryptocurrency.quote.USD.percent_change_7d))}%
-        </Typography>
-        </TableCell>
-        <TableCell component='th' scope='row'>
-          <Typography>
-            ${parseIntegerWithCommas(cryptocurrency.quote.USD.market_cap)}
-          </Typography>
-        </TableCell>
-        <TableCell component='th' scope='row'>
-          <Typography>
-            ${parseIntegerWithCommas(cryptocurrency.quote.USD.volume_24h)}
-          </Typography>
-        </TableCell>
-        <TableCell component='th' scope='row'>
-          <Typography>{`${parseIntegerWithCommas(cryptocurrency.circulating_supply)} (${cryptocurrency.symbol})`}</Typography>
-          <ProgressBar
-            fillColor={colors.lightGraySolid}
-            borderColor="black"
-            height="5px"
-            width="200px"
-            percent={(cryptocurrency.circulating_supply / cryptocurrency.total_supply) * 100} />
-        </TableCell>
-      </TableRow>
-    ))
+          </TableCell>
+          <TableCell component='th' scope='row'>
+            <Typography>
+              ${market_cap}
+            </Typography>
+          </TableCell>
+          <TableCell component='th' scope='row'>
+            <Typography>
+              ${volume_24h}
+            </Typography>
+          </TableCell>
+          <TableCell component='th' scope='row'>
+            <Typography>{`${circulating_supply} (${symbol})`}</Typography>
+            <ProgressBar
+              fillColor={colors.lightGraySolid}
+              borderColor="black"
+              height="5px"
+              width="200px"
+              percent={(circulating_supply / total_supply) * 100} />
+          </TableCell>
+        </TableRow>
+      )
+    })
   );
 };
 
